8634c6958a6e235bfadb45e7e84ce0f8a86c5de9
Turritopsis22 <acsf09@gmail.com>
2022-10-01 20:26:39 -0300

    Question resources added. Main screen fixed.

--- Main.gd
+++ Main.gd
@@ -1,9 +1,9 @@
 extends Control
 
-export var questions: Resource
+export var question_collection: Resource
 
 var current_question: int = 0
-onready var total_questions: int = questions.questions.size()
+onready var total_questions: int = question_collection.questions.size()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,11 +14,11 @@ func _next_question():
 	if current_question >= total_questions - 1:
 		print("end")
 	else:
-		$lblQuestion.text = questions.questions[current_question].question
-		$btnChoice1_Parent/btnChoice1.text = questions.questions[current_question].answers[0]
-		$btnChoice2_Parent/btnChoice2.text = questions.questions[current_question].answers[1]
-		$btnChoice3_Parent/btnChoice3.text = questions.questions[current_question].answers[2]
-		#$btnChoice4_Parent/btnChoice4.text = questions.questions[current_question].answers[3]
+		$lblQuestion.text = question_collection.questions[current_question].question
+		$btnChoice1_Parent/btnChoice1.text = question_collection.questions[current_question].answers[0]
+		$btnChoice2_Parent/btnChoice2.text = question_collection.questions[current_question].answers[1]
+		$btnChoice3_Parent/btnChoice3.text = question_collection.questions[current_question].answers[2]
+		#$btnChoice4_Parent/btnChoice4.text = question_collection.questions[current_question].answers[3]
 	
 	
 func _check_answer(p_question: Question, p_answer: int) -> bool:
@@ -36,28 +36,28 @@ func _show_popup_wrong():
 
 
 func _on_btnChoice1_button_up():
-	if _check_answer(questions.questions[current_question], 0):
+	if _check_answer(question_collection.questions[current_question], 0):
 		_show_popup_correct()
 	else:
 		_show_popup_wrong()
 
 
 func _on_btnChoice2_button_up():
-	if _check_answer(questions.questions[current_question], 1):
+	if _check_answer(question_collection.questions[current_question], 1):
 		_show_popup_correct()
 	else:
 		_show_popup_wrong()
 
 
 func _on_btnChoice3_button_up():
-	if _check_answer(questions.questions[current_question], 2):
+	if _check_answer(question_collection.questions[current_question], 2):
 		_show_popup_correct()
 	else:
 		_show_popup_wrong()
 
 
 #func _on_btnChoice4_button_up():
-#	if _check_answer(questions.questions[current_question], 3):
+#	if _check_answer(question_collection.questions[current_question], 3):
 #		_show_popup_correct()
 #	else:
 #		_show_popup_wrong()
